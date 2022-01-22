01e1eb7751888f15e32e70a67c3ea9be855b082a
Loser00000 <3036537400@qq.com>
2022-01-22 12:52:22 +0000

    第一个Java语言程序

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+//这是java的快速入门，演示java的开发步骤
+//对代码的相关说明
+//1.public class hello 表示Hello是一个类，是一个public公有的类
+//2.hello{} 表示一个类的开始和结束
+//3.public static void main(string[] args) 表示一个主方法，及我们程序的入口
+//4.main() {} 表示方法的开始和结束
+//5.System.out.println("Hello,world~"); 表示输出"hello,world~"到屏幕
+//6.;表示语句的结束
+public class HelloWorld{
+
+	//编写一个main方法
+	public static void main(String[] args){
+		System.out.println("Hello,world~");
+	}
+}
