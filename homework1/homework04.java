5f6d294df215cf8e0507e57db1291fa7f44f4d80
Loser00000 <3036537400@qq.com>
2022-01-26 08:02:34 +0000

    作业_1

---
+++ homework1/homework04.java
@@ -0,0 +1,4 @@
+//环境变量path配置及其作用
+1.环境变量的作用是为了再dos的任意目录，可以去使用java和javac命令
+2.先配置JAVA_HOME = 指向JDK安装的主目录
+3.编辑path环境变量，增加JAVA_HOME
---
+++ homework1/homework05.java
@@ -0,0 +1,4 @@
+//Java编写步骤
+1.编写java源代码
+2.javac 编译，得到对应的.clss字节码文件
+3.java运行，本质就是吧.clss加载到JVM运行
