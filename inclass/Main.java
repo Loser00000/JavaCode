4516c7b3b46e146c25d26e67e92ae782b4c0dccb
zding404 <89718192+Meguminn0@users.noreply.github.com>
2022-07-05 21:50:11 +0800

    作业_3
     编写老师类：
        (1) 要求有属性"姓名 name" "年龄 age" "职称 post" "基本工资 salary"
        (2) 编写业务方法，introduce(), 实现那输出一个教师的信息
        (3) 编写教师类的三个子类: 教授类，副教授类吗，讲师类。工资级别分别为：
            教授为1.3、副教授为1.2、讲师为1.1。在三个子类里面都重写父类的introduce() 方法
        (4) 定义并初始化一个老师对象，调用业务方法，实现对象基本信息的后台打印

--- inclass/Main.java
+++ inclass/Main.java
@@ -1,50 +1,45 @@
 package com.inclass;
 
 /*
-    定义一个Person类{name. age. job}.初始化Person对象数组。
-    有3个person对象。并按照age从大到小进行排序，提示：使用冒泡排序
+    编写老师类：
+    (1) 要求有属性"姓名 name" "年龄 age" "职称 post" "基本工资 salary"
+    (2) 编写业务方法，introduce(), 实现那输出一个教师的信息
+    (3) 编写教师类的三个子类: 教授类，副教授类吗，讲师类。工资级别分别为：
+        教授为1.3、副教授为1.2、讲师为1.1。在三个子类里面都重写父类的introduce() 方法
+    (4) 定义并初始化一个老师对象，调用业务方法，实现对象基本信息的后台打印
  */
+
+import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args) {
-        Person[] person = {new Person("jjj", 17, "w"),
-                new Person("kkk", 15, "jkkj"),
-                new Person("jklj", 44, "jkjkjk")};
-
-        for (int i = 0; i < person.length - 1; i++) {
-            Boolean flag = true;
-            for (int j = 0; j < person.length - 1 - i; j++) {
-                if(person[j].getAge() < person[j + 1].getAge()) {
-                    Person temp = person[j];
-                    person[j] = person[j + 1];
-                    person[j + 1] = temp;
-                    flag = false;
-                }
-            }
-            if(flag == true){
-                break;
-            }
-        }
-
-        for (int i = 0; i < person.length; i++) {
-            System.out.println(person[i].info());
-        }
-
-        for (int i = 0; i < person.length; i++) {
-            System.out.println(person[i].toString());
-        }
+        Lecturer teather = new Lecturer("王月", 42, "高级讲师", 7000, 1.1);
+        teather.introduce();
     }
 }
 
-class Person {
+class Teather{
     private String name;
     private int age;
-    private String job;
+    private String post;
+    private double salary;
+    private double salaryExp;
 
-    public Person(String name, int age, String job) {
+
+    public Teather(String name, int age, String post, double salaty, double salaryExp) {
         this.name = name;
         this.age = age;
-        this.job = job;
+        this.post = post;
+        this.salary = salaty;
+        this.salaryExp = salaryExp;
+    }
+
+    public double getSalaryExp() {
+        return salaryExp;
+    }
+
+    public void setSalaryExp(double salaryExp) {
+        this.salaryExp = salaryExp;
     }
 
     public String getName() {
@@ -63,15 +58,60 @@ class Person {
         this.age = age;
     }
 
-    public String getJob() {
-        return job;
+    public String getPost() {
+        return post;
     }
 
-    public void setJob(String job) {
-        this.job = job;
+    public void setPost(String post) {
+        this.post = post;
     }
 
-    public String info(){
-        return getName() + "\t" +  getAge() + "\t" + getJob();
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void introduce(){
+        DecimalFormat fom = new DecimalFormat("#0.00");
+        System.out.println("姓名: " + getName() +
+                "\n年   龄: " + getAge() +
+                "\n职   称: " + getPost() +
+                "\n工   资: " + fom.format(getSalary() * getSalaryExp()));
+    }
+}
+
+class Professor extends Teather{
+    public Professor(String name, int age, String post, double salary, double salaryExp) {
+        super(name, age, post, salary, salaryExp);
+    }
+
+    public void introduce(){
+        System.out.print("教授");
+        super.introduce();
+    }
+}
+
+class AssProfessor extends Teather{
+    public AssProfessor(String name, int age, String post, double salary, double salaryExp) {
+        super(name, age, post, salary, salaryExp);
+    }
+
+    public void introduce(){
+        System.out.print("副教授");
+        super.introduce();
+    }
+}
+
+class Lecturer extends Teather{
+    public Lecturer(String name, int age, String post, double salary, double salaryExp) {
+        super(name, age, post, salary, salaryExp);
+    }
+
+    public void introduce(){
+        System.out.print("讲师");
+        super.introduce();
     }
 }
